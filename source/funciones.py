9a26716dda0c47414ee5e8ab6a21028e1eeb5457
Lider Máximo <llopezraccioppe@danielcastelao.org>
2024-03-11 08:09:29 +0100

    pre examen

--- source/funciones.py
+++ source/funciones.py
@@ -1,3 +1,4 @@
+import doctest
 def nombre_de_la_funcion(parametro1, parametro2):
     """
     Funcion de ejemplo para mostar como se escriben las funciones.
@@ -11,6 +12,22 @@ def nombre_de_la_funcion(parametro1, parametro2):
     """
     print(parametro1)
     return parametro1+parametro2
+
+# prueba de modulo
+def cuadrados (lista):
+    """
+    Funcion que hace el cuadrado de los elementos de una lista
+    :param lista: lista de numeros
+    :return: lista de cuadrados
+
+    >>> lista = [1,2,3,4]
+    >>> cuadrados(lista)
+    [1, 4, 9, 16]
+    """
+    return [n ** 2 for n in lista]
+
+def _test():
+    doctest.testmod()
 
 print(nombre_de_la_funcion(1,2))
 print(nombre_de_la_funcion("hola ","que tal"))
